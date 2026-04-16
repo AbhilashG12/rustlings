f0ad57ea35aec9269a088e47475f4263807d9c8e
AbhilashG12 <abhilashggg15@gmail.com>
2026-04-16 20:32:21 +0530

    Exercise 11

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -1,10 +1,3 @@
-// A list of scores (one per line) of a soccer match is given. Each line is of
-// the form "<team_1_name>,<team_2_name>,<team_1_goals>,<team_2_goals>"
-// Example: "England,France,4,2" (England scored 4 goals, France 2).
-//
-// You have to build a scores table containing the name of the team, the total
-// number of goals the team scored, and the total number of goals the team
-// conceded.
 
 use std::collections::HashMap;
 
