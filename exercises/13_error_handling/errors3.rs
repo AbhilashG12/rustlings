9015c0120ace601910a51daa2e49ca610dc528fd
AbhilashG12 <abhilashggg15@gmail.com>
2026-05-02 20:46:54 +0530

    Exercise 13

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -16,13 +16,17 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    // Don't change this line.
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {tokens} tokens.");
+    match total_cost(pretend_user_input) {
+        Ok(cost) => {
+            if cost > tokens {
+                println!("You can't afford that many!");
+            } else {
+                tokens -= cost;
+                println!("You now have {} tokens.", tokens);
+            }
+        },
+        Err(e) => panic!("{}",e)
     }
+
+    
 }
